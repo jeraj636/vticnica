3e4e7066a03b406d6d2b4aa4b2d787dade1f6b10
jeraj636 <jakob.jeraj7@gmail.com>
2025-02-21 21:47:36 +0100

    zadeva naj bi delovala

--- grebator.js
+++ grebator.js
@@ -1,15 +1,19 @@
+/*
+Preprosta koda, ki iz dveh api-jev beleži podatke v postgres.
+*/
+
 const pg = require("pg");
 const fs = require("fs");
-const { findSourceMap } = require("module");
+
 let debug = false;
+
+//* Naslovi api-jev
 const vticnica_ip = "http://192.168.1.42/rpc/Shelly.GetStatus";
 const api_link = 'https://api-drzavno-test.scv.si/api/tarifa';
 
-
-
+//* Razred, ki hrani podatke iz dveh API-jev
 class zdruzeni_podatki {
     constructor(vticnica_json, tarifa_json) {
-
         this.stanje = vticnica_json['switch:0'].output;
         this.napetost = vticnica_json['switch:0'].voltage;
         this.frekvenca = vticnica_json['switch:0'].freq;
@@ -19,7 +23,6 @@ class zdruzeni_podatki {
         this.tarifa = tarifa_json.tarifa;
         this.valuta = tarifa_json.valuta;
         this.casovni_blok = tarifa_json.casovni_blok;
-
     }
     izpisi() {
         console.log("-----------------------------------------------------------------------------------------------------");
@@ -54,8 +57,22 @@ class zdruzeni_podatki {
 
         const rezultat = await odjemalec.query(poizvedba, vrednosti);
     }
-}
 
+}
+//Funkcija, ki brise podatke, ki so straejši od 24ur iz PB
+async function izbrisi_stare_podatke(odjemalec) {
+    const danes = new Date().toISOString().split('T')[0];
+    const poizvedba = `
+    DELETE FROM stanje_vticnice
+    WHERE casovni_zig < NOW() - INTERVAL '24 hours'`;
+    try {
+        await odjemalec.query(poizvedba, [danes]);
+        console.log(`Brisanje zapisov!`);
+    } catch (error) {
+        console.error('Napaka:', error);
+    }
+}
+// Funkcija ki prebere podatke iz API
 async function dobi_json_iz_povezave(povezava) {
     try {
         const odgovor = await fetch(povezava);
@@ -65,16 +82,17 @@ async function dobi_json_iz_povezave(povezava) {
         const podatki = await odgovor.json();
         return podatki;
     } catch (error) {
-        console.error("Napaka pri pridobivanju podatkov:", error);
+        console.error("Napaka: ", error);
     }
 }
+
 let pg_odjemalec;
 fs.readFile('nastavitve.json', function (err, podatki) {
     pg_odjemalec = new pg.Client(JSON.parse(podatki));
     pg_odjemalec.connect();
 
 });
-const interval = setInterval(async () => {
+const branje_podatkov = setInterval(async () => {
     try {
 
         const vticnica_json = await dobi_json_iz_povezave(vticnica_ip);
@@ -89,6 +107,18 @@ const interval = setInterval(async () => {
     }
 
 }, 500);
+const brisanje_podatkov = setInterval(async () => {
+    try {
+        izbrisi_stare_podatke(pg_odjemalec);
+
+    } catch (error) {
+        console.error('Napaka:', error);
+        process.exit();
+    }
+
+}, 60 * 1000);
+
+//* Konec Programa
 process.on('SIGINT', async () => {
     clearInterval(interval);
     await pg_odjemalec.end();
